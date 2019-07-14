6edc121d59ea435487a2eea3e527c158cb58d2d3
jenish patel <fenilshah288@gmail.com>
2019-07-14 19:42:08 -0400

    small change

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ export default class App extends React.Component {
         humidity : undefined,
         description : undefined,
         error : undefined
+        //git testing
         
     }
 
